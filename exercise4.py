ae4e11333d021733bea160e63761619f35031714
LOOIKAHYAN <looikahyan0202@gmail.com>
2024-04-18 17:16:17 +0800

    exercises

---
+++ exercise4.py
@@ -0,0 +1,5 @@
+from math import pi
+
+r = float(input("r = "))
+area = pi*r*r
+print("Area = "+str(area))
---
+++ exercise5.py
@@ -0,0 +1,3 @@
+first_name = input("First Name: ")
+last_name = input("Last Name: ")
+print(last_name + ' ' + first_name)
